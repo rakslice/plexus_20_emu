d5c09f5e660db2adbaffca592174aa76819257e3
rakslice <rakslice@gmail.com>
2024-06-24 12:18:47 +0000

    in supervisor mode allow access to mapped memory if the uid is different

--- mapper.c
+++ mapper.c
@@ -64,7 +64,7 @@ static int access_allowed_page(mapper_t *m, unsigned int page, int access_flags)
 	unsigned int ac=(m->desc[page].w1<<16)+m->desc[page].w0;
 	int fault=(ac&access_flags)&(ACCESS_R|ACCESS_W|ACCESS_X);
 	int uid=(ac>>W0_UID_SHIFT)&W0_UID_MASK;
-	if (uid != m->cur_id) fault=(uid<<8|0xff);
+	if (uid != m->cur_id && (access_flags&ACCESS_SYSTEM)==0) fault=(uid<<8|0xff);
 	if (fault) {
 		MAPPER_LOG_DEBUG("Mapper: Access fault at page %d, page addr %x, fault %x (page ent %x req %x)\n", page, (page&2047)<<12, fault, ac, access_flags);
 		dump_cpu_state();
